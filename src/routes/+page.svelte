a45820afc796436e2910ead7c02dca512ea9a9b6
veniplex <19841573+veniplex@users.noreply.github.com>
2024-07-07 20:24:57 +0200

    fixed #18 - Google authentication

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,33 @@
 <script>
 	import { onMount } from "svelte";
 	import { auth, GoogleAuthProvider } from "$lib/firebase";
-	import { signInWithRedirect, getRedirectResult } from "firebase/auth";
+	import { signInWithRedirect, signInWithPopup } from "firebase/auth";
 	import { goto } from "$app/navigation";
 
-	function loginWithGoogle() {
-		const provider = new GoogleAuthProvider();
-		signInWithRedirect(auth, provider);
-	}
+	// SignInWithRedirect method (deprecated)
+	// function loginWithGoogle() {
+	// 	const provider = new GoogleAuthProvider();
+	// 	signInWithRedirect(auth, provider);
+	// }
 
-	onMount(async () => {
-		const result = await getRedirectResult(auth);
-		if (result) {
+	// onMount(async () => {
+	// 	const result = await getRedirectResult(auth);
+	// 	if (result) {
+	// 		goto("/dashboard");
+	// 	} else {
+	// 		auth.onAuthStateChanged((user) => {
+	// 			if (user) {
+	// 				goto("/dashboard");
+	// 			}
+	// 		});
+	// 	}
+	// });
+
+	// SignInWithPopup (not working on mobile)
+	async function loginWithGoogle() {
+		const provider = new GoogleAuthProvider();
+		const credentials = await signInWithPopup(auth, provider);
+		if (credentials) {
 			goto("/dashboard");
 		} else {
 			auth.onAuthStateChanged((user) => {
@@ -20,11 +36,11 @@
 				}
 			});
 		}
-	});
+	}
 </script>
 
 <div class="flex flex-col m-auto h-fit w-fit justify-center items-center">
-	<img src="/couple-quest-logo.png" class="" alt="Couple Quest Logo" />
+	<img src="/couple-quest-logo.png" class="max-w-screen-sm" alt="Couple Quest Logo" />
 	<button class="btn btn-primary sm:btn-wide btn-md sm:btn-lg" on:click={loginWithGoogle}
 		>Mit Google anmelden</button
 	>
